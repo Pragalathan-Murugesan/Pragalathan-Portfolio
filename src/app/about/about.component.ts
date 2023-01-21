bf85afd7070deb8e63456b63da29289439cd9594
Pragalathan <90542273+Pragalathan-Murugesan@users.noreply.github.com>
2023-01-21 08:37:53 +0530

    new changes

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-  pdfSrc =  "../assets/Images/Pragalathan_Resume.pdf";
+  pdfSrc =  "../assets/Images/Pragalathan M_resume.pdf";
   status: 'active' | 'inactive' = 'inactive';
 
   constructor() { }
